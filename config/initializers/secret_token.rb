c9a217f0543a749eab55080cac6be07051a13c30
Jay Pinho <jaypinho@gmail.com>
2015-03-12 12:52:05 -0400

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Newsmeter::Application.config.secret_key_base = 'a56bd73034a2d20e044a7194f6f71148ddb844b35de652c5f14523d898096a2b444321cc750a7f6ba0703b681f14ead07d8676584ba49596ae640bec1d43c810'
