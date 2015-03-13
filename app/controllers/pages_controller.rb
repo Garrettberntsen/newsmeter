70dfa96f37843f570826a6255cf0a89e3d9a35ec
Jay Pinho <jaypinho@gmail.com>
2015-03-13 00:39:30 -0400

    Default to null if no value is passed

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,7 +5,7 @@ class PagesController < ApplicationController
 	protect_from_forgery except: :send_page
 
 	def send_page
-		new_page = Page.new :user_id => 1, :actual_url => params[:url], :og_url => params[:ogurl], :og_title => params[:ogtitle], :og_type => params[:ogtype] 
+		new_page = Page.new :user_id => 1, :actual_url => (params[:url] == "" ? nil; params[:url]), :og_url => (params[:ogurl] == "" ? nil; params[:ogurl]), :og_title => (params[:ogtitle] == "" ? nil; params[:ogtitle]), :og_type => (params[:ogtype == "" ? nil; params[:ogtype]) 
 		new_page.save
 		# response.headers.delete('X-Frame-Options')
 		render :nothing => true, :status => 204
