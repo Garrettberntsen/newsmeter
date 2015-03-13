6225cc758928206b3d4c9602b25a4dab3dfd6835
Jay Pinho <jaypinho@gmail.com>
2015-03-13 09:52:25 -0400

    Add error handling to DB save

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,10 +5,16 @@ class PagesController < ApplicationController
 	protect_from_forgery except: :send_page
 
 	def send_page
-		new_page = Page.new :user_id => 1, :actual_url => (params[:url] == "" ? nil : params[:url]), :og_url => (params[:ogurl] == "" ? nil : params[:ogurl]), :og_title => (params[:ogtitle] == "" ? nil : params[:ogtitle]), :og_type => (params[:ogtype] == "" ? nil : params[:ogtype]) 
-		new_page.save
-		# response.headers.delete('X-Frame-Options')
-		render :nothing => true, :status => 204
+
+		begin
+			new_page = Page.new :user_id => 1, :actual_url => (params[:url] == "" ? nil : params[:url]), :og_url => (params[:ogurl] == "" ? nil : params[:ogurl]), :og_title => (params[:ogtitle] == "" ? nil : params[:ogtitle]), :og_type => (params[:ogtype] == "" ? nil : params[:ogtype]) 
+			new_page.save
+			# response.headers.delete('X-Frame-Options')
+			render :nothing => true, :status => 204
+		rescue => e
+			render :nothing => true, :status => 204
+		end
+
 	end
 
 end
