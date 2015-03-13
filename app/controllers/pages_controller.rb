7c86fb6819f9b59479451248217ffa2042f1656d
Jay Pinho <jaypinho@gmail.com>
2015-03-13 00:20:04 -0400

    Turn off authenticity token

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,10 +2,12 @@ class PagesController < ApplicationController
 
 	layout false
 
+	protect_from_forgery except: :send_page
+
 	def send_page
 		new_page = Page.new :user_id => 1, :actual_url => params[:url], :og_url => params[:ogurl], :og_title => params[:ogtitle], :og_type => params[:ogtype] 
 		new_page.save
-		response.headers.delete('X-Frame-Options')
+		# response.headers.delete('X-Frame-Options')
 		render :nothing => true, :status => 204
 	end
 
