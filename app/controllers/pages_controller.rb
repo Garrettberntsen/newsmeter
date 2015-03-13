1c45081751c3de24535f2a784a1d489b57074767
Jay Pinho <jaypinho@gmail.com>
2015-03-13 10:14:54 -0400

    Don't create error for get request

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -6,6 +6,10 @@ class PagesController < ApplicationController
 
 	def send_page
 
+		if request.get?
+			render :nothing => true, :status => 204 and return
+		end
+
 		begin
 			new_page = Page.new :user_id => 1, :actual_url => (params[:url] == "" ? nil : params[:url]), :og_url => (params[:ogurl] == "" ? nil : params[:ogurl]), :og_title => (params[:ogtitle] == "" ? nil : params[:ogtitle]), :og_type => (params[:ogtype] == "" ? nil : params[:ogtype]) 
 			new_page.save
