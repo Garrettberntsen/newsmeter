222e1a09c696b8a40055c2c055c81aafd042b1f0
Jay Pinho <jaypinho@gmail.com>
2015-03-13 23:46:57 -0400

    Check for site whitelist before saving page visit

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,6 +11,13 @@ class PagesController < ApplicationController
 		end
 
 		begin
+			whitelist = Site.select(:domain).pluck(:domain)
+			is_whitelisted = false
+			whitelist.each do |x|
+				is_whitelisted = true if URI.parse(params[:url]).host.include? x
+			end
+			render :nothing => true, :status => 204 and return if is_whitelisted == false
+
 			new_page = Page.new :user_id => 1, :actual_url => (params[:url] == "" ? nil : params[:url]), :og_url => (params[:ogurl] == "" ? nil : params[:ogurl]), :og_title => (params[:ogtitle] == "" ? nil : params[:ogtitle]), :og_type => (params[:ogtype] == "" ? nil : params[:ogtype]), :domain => (params[:url] == "" ? nil : URI.parse(params[:url]).host)
 			new_page.save
 			# response.headers.delete('X-Frame-Options')
