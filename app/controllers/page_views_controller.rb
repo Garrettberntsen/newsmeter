61a14ef62f09f1106632553a672014a6c5194c4e
Jay Pinho <jaypinho@gmail.com>
2015-03-15 22:47:42 -0400

    HOLY SHIT IT WAS ABOUT SCOPE THE WHOLE TIME

--- app/controllers/page_views_controller.rb
+++ app/controllers/page_views_controller.rb
@@ -17,7 +17,7 @@ class PageViewsController < ApplicationController
 			whitelist.each do |x|
 				if URI.parse(params[:url]).host.downcase.include? x.domain
 					is_whitelisted = true
-					site_id = x
+					site_id = x.id
 				end
 			end
 			render :nothing => true, :status => 204 and return if is_whitelisted == false
@@ -26,8 +26,7 @@ class PageViewsController < ApplicationController
 				new_page_view = PageView.new :user_id => 1, :actual_url => (params[:url] == "" ? nil : params[:url]), :page_id => (Page.find_by canonical_url: params[:canonicalurl]).id
 				new_page_view.save
 			else
-				new_page = Page.new :canonical_url => params[:canonicalurl], :title => (params[:canonicaltitle] == "" ? nil: params[:canonicaltitle]), :page_type => (params[:ogtype] == "" ? nil: params[:ogtype]), :author => (params[:author] == "" ? nil: params[:author]), :keywords => (params[:keywords] == "" ? nil: params[:keywords]), :description => (params[:description] == "" ? nil: params[:description]) 
-				new_page.site = site_id
+				new_page = Page.new :canonical_url => params[:canonicalurl], :title => (params[:canonicaltitle] == "" ? nil: params[:canonicaltitle]), :page_type => (params[:ogtype] == "" ? nil: params[:ogtype]), :author => (params[:author] == "" ? nil: params[:author]), :keywords => (params[:keywords] == "" ? nil: params[:keywords]), :description => (params[:description] == "" ? nil: params[:description]), :site_id => site_id 
 				new_page.save
 				new_page_view = PageView.new :user_id => 1, :actual_url => (params[:url] == "" ? nil : params[:url]), :page_id => (Page.find_by canonical_url: params[:canonicalurl]).id
 				new_page_view.save
