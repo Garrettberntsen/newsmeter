f20fa45f41207ecfedf2f36838bddf03af539f4b
Jay Pinho <jaypinho@gmail.com>
2015-03-15 22:35:44 -0400

    Try different reference type

--- app/controllers/page_views_controller.rb
+++ app/controllers/page_views_controller.rb
@@ -16,22 +16,17 @@ class PageViewsController < ApplicationController
 			whitelist.each do |x|
 				if URI.parse(params[:url]).host.downcase.include? x.domain
 					is_whitelisted = true
-					site_id = x.id
+					site_id = x
 				end
 			end
 			render :nothing => true, :status => 204 and return if is_whitelisted == false
 
-			puts "GOT THROUGH SITE WHITELIST"
-
 			if Page.select(:canonical_url).pluck(:canonical_url).include? params[:canonicalurl]
-				puts "PAGE EXISTED ALREADY"
 				new_page_view = PageView.new :user_id => 1, :actual_url => (params[:url] == "" ? nil : params[:url]), :page_id => (Page.find_by canonical_url: params[:canonicalurl]).id
 				new_page_view.save
 			else
-				puts "PAGE DIDN'T EXIST ALREADY"
-				new_page = Page.new :canonical_url => params[:canonicalurl], :title => (params[:canonicaltitle] == "" ? nil: params[:canonicaltitle]), :page_type => (params[:ogtype] == "" ? nil: params[:ogtype]), :author => (params[:author] == "" ? nil: params[:author]), :keywords => (params[:keywords] == "" ? nil: params[:keywords]), :description => (params[:description] == "" ? nil: params[:description])
+				new_page = Page.new :canonical_url => params[:canonicalurl], :title => (params[:canonicaltitle] == "" ? nil: params[:canonicaltitle]), :page_type => (params[:ogtype] == "" ? nil: params[:ogtype]), :author => (params[:author] == "" ? nil: params[:author]), :keywords => (params[:keywords] == "" ? nil: params[:keywords]), :description => (params[:description] == "" ? nil: params[:description]), :site => x 
 				new_page.save
-				puts "NEW PAGE CREATED; NOW MOVING ONTO CREATING PAGE VIEW"
 				new_page_view = PageView.new :user_id => 1, :actual_url => (params[:url] == "" ? nil : params[:url]), :page_id => (Page.find_by canonical_url: params[:canonicalurl]).id
 				new_page_view.save
 			end
