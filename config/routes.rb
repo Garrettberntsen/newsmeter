43e8f600f535ebe6f5cc7fd0ca98b9af074d95bd
Jay Pinho <jaypinho@gmail.com>
2015-03-20 00:22:29 -0400

    More Devise and OAuth setup

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
 
-  devise_for :users
+  devise_for :users, :controllers => { omniauth_callbacks: 'omniauth_callbacks' }
   # resources :pages
 
   match "send_page" => "page_views#send_page", via: [:get, :post]
