564afa453295129aca69e792ca185e72998dc45d
Jay Pinho <jaypinho@gmail.com>
2015-03-22 14:19:12 -0400

    Fix schema.rb

--- db/schema.rb
+++ db/schema.rb
@@ -16,14 +16,6 @@ ActiveRecord::Schema.define(version: 20150322181243) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
-  create_table "identities", force: :cascade do |t|
-    t.integer  "user_id",    index: {name: "index_identities_on_user_id"}
-    t.string   "provider"
-    t.string   "uid"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
   create_table "keywords", force: :cascade do |t|
     t.string   "keyword",    limit: 255, index: {name: "index_keywords_on_keyword", unique: true, case_sensitive: false}
     t.datetime "created_at"
@@ -67,25 +59,4 @@ ActiveRecord::Schema.define(version: 20150322181243) do
     t.datetime "updated_at"
   end
 
-  create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false, index: {name: "index_users_on_email", unique: true}
-    t.string   "encrypted_password",     default: "", null: false
-    t.string   "reset_password_token",   index: {name: "index_users_on_reset_password_token", unique: true}
-    t.datetime "reset_password_sent_at"
-    t.datetime "remember_created_at"
-    t.integer  "sign_in_count",          default: 0,  null: false
-    t.datetime "current_sign_in_at"
-    t.datetime "last_sign_in_at"
-    t.inet     "current_sign_in_ip"
-    t.inet     "last_sign_in_ip"
-    t.string   "confirmation_token"
-    t.datetime "confirmed_at"
-    t.datetime "confirmation_sent_at"
-    t.string   "unconfirmed_email"
-    t.datetime "created_at"
-    t.datetime "updated_at"
-    t.string   "name"
-  end
-
-  add_foreign_key "identities", "users"
 end
