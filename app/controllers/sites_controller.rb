2ac1fbf4d68f025b9e1065dcb53e852aa29d515e
Jay Pinho <jaypinho@gmail.com>
2015-03-23 18:00:57 -0400

    Format sites list

--- app/controllers/sites_controller.rb
+++ app/controllers/sites_controller.rb
@@ -2,7 +2,7 @@ class SitesController < ApplicationController
 
 	def index
 		@sites = Site.all
-		render :text => @sites.pluck(:domain).join(', ')
+		render :text => @sites.pluck(:domain).sort_by{|x, y| x <=> y }.join('\n')
 	end
 
 end
