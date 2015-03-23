6a3de7e2e9f6e2280827eebd8ae0a8f777947d51
Jay Pinho <jaypinho@gmail.com>
2015-03-23 01:10:14 -0400

    Add detail

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -7,7 +7,7 @@ class SessionsController < ApplicationController
 
   def create
   	auth_hash = request.env['omniauth.auth']
-  	puts auth_hash.extra.raw_info.friends.inspect
+  	x = auth_hash.extra.raw_info.friends.data.inspect
 
   	if session[:user_id]
   		# Means our user is signed in. Add the authorization to the user
@@ -21,7 +21,7 @@ class SessionsController < ApplicationController
   		# Create the session
   		session[:user_id] = auth.user.id
 
-  		render :text => "Welcome #{auth.user.name}!"
+  		render :text => "Welcome #{auth.user.name}! Also: #{x}"
   	end
   end
 
