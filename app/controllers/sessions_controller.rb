d1394033ed2b492be2cefe3c7c85c8cf542a5e32
Jay Pinho <jaypinho@gmail.com>
2015-03-22 21:25:09 -0400

    Try again

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -19,7 +19,7 @@ class SessionsController < ApplicationController
 
   		# Create the session
   		session[:user_id] = auth.user.id
-  		puts ("HERE'S THE USER ID: " + auth.user.id)
+  		puts ("HERE'S THE USER ID: " + auth.user.id.to_s)
   		puts ("HERE'S THE SESSION ID: " + session[:user_id])
 
   		render :text => "Welcome #{auth.user.name}!"
