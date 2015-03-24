7bed274db7a8c8ae09e34b0172f93d95a4c48a84
Jay Pinho <jaypinho@gmail.com>
2015-03-23 21:40:43 -0400

    Return name and email too

--- config/initializers/omniauth.rb
+++ config/initializers/omniauth.rb
@@ -1,3 +1,3 @@
 Rails.application.config.middleware.use OmniAuth::Builder do
-	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {:scope => 'public_profile,email,user_friends', :info_fields => 'friends'}
+	provider :facebook, ENV['FACEBOOK_KEY'], ENV['FACEBOOK_SECRET'], {:scope => 'public_profile,email,user_friends', :info_fields => 'name,email,friends'}
 end
