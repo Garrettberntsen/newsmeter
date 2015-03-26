795662a9a3ed93d2e7796b05338e8e71fa3122eb
Jay Pinho <jaypinho@gmail.com>
2015-03-25 22:47:37 -0400

    De-nil

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -8,7 +8,7 @@ class SessionsController < ApplicationController
   def create
   	auth_hash = request.env['omniauth.auth']
   	x = auth_hash.extra.raw_info.friends.data
-    z = nil
+    z = ""
     x.each do |y|
       z += y['name'] + " (" + y['id'] +")<br>"
     end
