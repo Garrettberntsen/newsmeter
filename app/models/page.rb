ac3ab8061205217f6c47b51477b63a8ef1e31bcd
Jay Pinho <jaypinho@gmail.com>
2015-03-29 01:41:18 -0400

    Add search scope association

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,6 +1,7 @@
 class Page < ActiveRecord::Base
   include PgSearch
   multisearchable :against => [:title, :author, :description]
+  pg_search_scope :find_relevant_pages, :against => [:title, :author, :description], :associated_against => { :keywords => [:keyword] }
 
   belongs_to :site
   has_many :keywords, through: :page_keywords
