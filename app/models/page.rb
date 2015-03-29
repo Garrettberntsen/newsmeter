df5ac4f0e33a52f7d8d0ed46718648320fb8ac40
Jay Pinho <jaypinho@gmail.com>
2015-03-29 13:38:54 -0400

    Change weights once again

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ActiveRecord::Base
   include PgSearch
   multisearchable :against => [:title, :author, :description]
-  pg_search_scope :find_relevant_pages, :against => [ [:title, 'A'], [:author, 'D'], [:description, 'B'] ], :associated_against => { :keywords => {:keyword => 'C'} }, :using => { :tsearch => {:dictionary => "english", :normalization => 2} }
+  pg_search_scope :find_relevant_pages, :against => [ [:title, 'A'], [:author, 'D'], [:description, 'C'] ], :associated_against => { :keywords => {:keyword => 'B'} }, :using => { :tsearch => {:dictionary => "english", :normalization => 2} }
 
   belongs_to :site
   has_many :keywords, through: :page_keywords
