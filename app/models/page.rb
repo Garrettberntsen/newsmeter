7ef0f949e59784bfb53e7b1e3cb63cf1e2b962e0
Jay Pinho <jaypinho@gmail.com>
2015-03-29 01:55:47 -0400

    Syntax fix

--- app/models/page.rb
+++ app/models/page.rb
@@ -1,7 +1,7 @@
 class Page < ActiveRecord::Base
   include PgSearch
   multisearchable :against => [:title, :author, :description]
-  pg_search_scope :find_relevant_pages, :against => [ [:title, 'A'], [:author, 'B'], [:description, 'D'] ], :associated_against => { :keywords => [:keyword, 'C'] }, :using => { :tsearch => {:dictionary => "english"} }
+  pg_search_scope :find_relevant_pages, :against => [ [:title, 'A'], [:author, 'B'], [:description, 'D'] ], :associated_against => { :keywords => {:keyword => 'C'} }, :using => { :tsearch => {:dictionary => "english"} }
 
   belongs_to :site
   has_many :keywords, through: :page_keywords
