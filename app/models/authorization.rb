4519e1b8943b4c8c565d565f607a2f147693e7ab
Jay Pinho <jaypinho@gmail.com>
2015-05-19 21:58:35 -0400

    Debug again

--- app/models/authorization.rb
+++ app/models/authorization.rb
@@ -4,6 +4,8 @@ class Authorization < ActiveRecord::Base
 
 	def self.find_or_create(auth_hash)
 		unless auth = find_by_provider_and_uid(auth_hash["provider"], auth_hash["uid"])
+			puts auth_hash["info"]["name"]
+			puts auth_hash["info"]["email"]
 			user = User.create :name => (auth_hash["info"].has_key?("name") ? auth_hash["info"]["name"]: "" ), :email => (auth_hash["info"].has_key?("email") ? auth_hash["info"]["email"] : "")
 			auth = create :user => user, :provider => auth_hash["provider"], :uid => auth_hash["uid"]
 		end
